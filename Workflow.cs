024ffa1e47e817b92023842cf0874a8aee6c0916
Eduardo Gomes Sanglard <eduardo.sanglard@hotmail.com>
2024-09-08 23:37:33 -0300

    Att azure-pipelines

---
+++ Workflow.cs
@@ -0,0 +1,29 @@
+using MyTestProcess.ObjectRepository;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Orchestrator.Client.Models;
+using UiPath.Testing;
+using UiPath.Testing.Activities.TestData;
+using UiPath.Testing.Activities.TestDataQueues.Enums;
+using UiPath.Testing.Enums;
+
+namespace MyTestProcess
+{
+    public class Workflow : CodedWorkflow
+    {
+        [Workflow]
+        public void Execute()
+        {
+            // To start using services, use IntelliSense (CTRL + Space) to discover the available services:
+            // e.g. system.GetAsset(...)
+
+            // For accessing UI Elements from Object Repository, you can use the Descriptors class e.g:
+            // var screen = uiAutomation.Open(Descriptors.MyApp.FirstScreen);
+            // screen.Click(Descriptors.MyApp.FirstScreen.SettingsButton);
+        }
+    }
+}
